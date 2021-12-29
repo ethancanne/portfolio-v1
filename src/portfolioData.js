a9c396b734565bbc5eb7acd51f47fbfe0c7549f9
Ethan Cannelongo <ethancannelongo@Ethans-MacBook-Pro.local>
2021-12-28 19:32:05 -0500

    Added Gias hope portfolio

--- src/portfolioData.js
+++ src/portfolioData.js
@@ -1,143 +1,166 @@
 export const frontend = [
-    {
-        id: 1,
-        title: "Library Website",
-        description: "This website was developed and submitted as a graded assigment for a Web Architecture and Development class.  It showcases multiple aspects of my design capabilites with CSS and front-end architecture.",
-        dateCreated: "Dec 11, 2020",
-        link: "http://ethancannelongo.great-site.net/library-website",
-        img:"./assets/portfolio/LibraryWebsite.png"
-    },
-    {
-        id: 1,
-        title: "Computer Retail Website",
-        description: "This website was developed and submitted as a graded assigment for a Web Architecture and Development class.  It showcases my design skills along with my ability with using JQuery to apply functionality to the website, such as an interactive cart, popups, along with many smooth animations therein.",
-        dateCreated: "Nov 20, 2020",
-        link: "http://ethancannelongo.great-site.net/computer-retail-website",
-        img:"./assets/portfolio/ComputerRetailWebsite.png"
-    },
-    {
-        id: 2,
-        title: "Museum Website",
-        description: "This website was developed and submitted as a graded assigment for a Web Architecture and Development class.  It showcases my capability with applying animation within CSS and designing modern, interactive interfaces.",
-        dateCreated: "Oct 30, 2020",
-        link: "http://ethancannelongo.great-site.net/museum-website/",
-        img:"./assets/portfolio/MuseumWebsite.png"
-    },
-    {
-        id: 3,
-        title: "Hotel Website",
-        description: "This is a static website, which was developed and submitted as a graded assigment for a Web Architecture and Development class.  The design for this website was given in class as an image. This website showcases my capability with taking an image mockup and applying the design into an actual style for a website using CSS.",
-        dateCreated: "Oct 18, 2020",
-        link: "http://ethancannelongo.great-site.net/hotel-website/",
-        img:"./assets/portfolio/HotelWebsite.png"
-    },
-    {
-        id: 4,
-        title: "Coffee Website",
-        description: "This website was developed and submitted as a graded assigment for a Web Architecture and Development class.  It showcases multiple aspects of my design capabilites with CSS and front-end architecture.",
-        dateCreated: "Sept 28, 2020",
-        link: "http://ethancannelongo.great-site.net/coffee-website/",
-        img:"./assets/portfolio/CoffeeWebsite.png"
-    },
-    {
-        id: 5,
-        title: "Wildflower Hope",
-        description: "This blog was voluntarily developed using Wordpress for a non-profit organization.  It features a custom, modern design; email marketing integration; and many other features that showcase my ability with front-end architecture as well as utilizing a CMS, such as Wordpress",
-        dateCreated: "Oct 16, 2019",
-        link: "https://wildflowerhope.com",
-        img:"./assets/portfolio/WildflowerHopeWebsite.png"
-    },
-    {
-        id: 6,
-        title: "GitHub User Finder Website",
-        description: "This website was developed as a personal project using React.js as the front-end framework and bootstrap for the styling.  It utilizes the GitHub and Context APIs, to retrieve and store data and allow for the searching of users on GitHub.",
-        dateCreated: "Dec 23, 2020",
-        link: "https://github.com/ethancanne/github-finder",
-        img:"./assets/portfolio/GitHubUserFinderWebsite.png"
-    }
-]
+  {
+    id: 1,
+    title: "GIA'S Hope Website",
+    description:
+      "This website was voluntarily developed and deployed using React and Next.JS for a non-profit organization called GIA'S Hope.  The website features an intuitive and responsive user interface developed using the Sass design language, which is plesant and easy to use.  It incorporates advanced features including many fluid animations and effects.",
+    link: "https://www.giashope.org",
+    img: "./assets/portfolio/giashope.png",
+  },
+  {
+    id: 1,
+    title: "Library Website",
+    description:
+      "This website was developed and submitted as a graded assigment for a Web Architecture and Development class.  It showcases multiple aspects of my design capabilites with CSS and front-end architecture.",
+    dateCreated: "Dec 11, 2020",
+    link: "http://ethancannelongo.great-site.net/library-website",
+    img: "./assets/portfolio/LibraryWebsite.png",
+  },
+  {
+    id: 1,
+    title: "Computer Retail Website",
+    description:
+      "This website was developed and submitted as a graded assigment for a Web Architecture and Development class.  It showcases my design skills along with my ability with using JQuery to apply functionality to the website, such as an interactive cart, popups, along with many smooth animations therein.",
+    dateCreated: "Nov 20, 2020",
+    link: "http://ethancannelongo.great-site.net/computer-retail-website",
+    img: "./assets/portfolio/ComputerRetailWebsite.png",
+  },
+  {
+    id: 2,
+    title: "Museum Website",
+    description:
+      "This website was developed and submitted as a graded assigment for a Web Architecture and Development class.  It showcases my capability with applying animation within CSS and designing modern, interactive interfaces.",
+    dateCreated: "Oct 30, 2020",
+    link: "http://ethancannelongo.great-site.net/museum-website/",
+    img: "./assets/portfolio/MuseumWebsite.png",
+  },
+  {
+    id: 3,
+    title: "Hotel Website",
+    description:
+      "This is a static website, which was developed and submitted as a graded assigment for a Web Architecture and Development class.  The design for this website was given in class as an image. This website showcases my capability with taking an image mockup and applying the design into an actual style for a website using CSS.",
+    dateCreated: "Oct 18, 2020",
+    link: "http://ethancannelongo.great-site.net/hotel-website/",
+    img: "./assets/portfolio/HotelWebsite.png",
+  },
+  {
+    id: 4,
+    title: "Coffee Website",
+    description:
+      "This website was developed and submitted as a graded assigment for a Web Architecture and Development class.  It showcases multiple aspects of my design capabilites with CSS and front-end architecture.",
+    dateCreated: "Sept 28, 2020",
+    link: "http://ethancannelongo.great-site.net/coffee-website/",
+    img: "./assets/portfolio/CoffeeWebsite.png",
+  },
+  {
+    id: 5,
+    title: "Wildflower Hope",
+    description:
+      "This blog was voluntarily developed using Wordpress for a non-profit organization.  It features a custom, modern design; email marketing integration; and many other features that showcase my ability with front-end architecture as well as utilizing a CMS, such as Wordpress",
+    dateCreated: "Oct 16, 2019",
+    link: "https://wildflowerhope.com",
+    img: "./assets/portfolio/WildflowerHopeWebsite.png",
+  },
+  {
+    id: 6,
+    title: "GitHub User Finder Website",
+    description:
+      "This website was developed as a personal project using React.js as the front-end framework and bootstrap for the styling.  It utilizes the GitHub and Context APIs, to retrieve and store data and allow for the searching of users on GitHub.",
+    dateCreated: "Dec 23, 2020",
+    link: "https://github.com/ethancanne/github-finder",
+    img: "./assets/portfolio/GitHubUserFinderWebsite.png",
+  },
+];
 
 export const backend = [
-    {
-        id: 1,
-        title: "Task-Manager API",
-        description: "A designed, developed, and deployed complete Restful API that allows a user to create accounts and store personal tasks in a real-time database.  It utilizes React.js as the front-end framework with Node.js running an Express.js server on the backend.  It interacts with MongoDB for data storage and retrieval.",
-        dateCreated: "Feb 20, 2021",
-        link: "https://github.com/ethancanne/task-manager-api",
-        img:"./assets/portfolio/TaskManagerAPI.png"
-    },
-    {
-        id: 2,
-        title: "KYXA Game Websockets API",
-        description: "This online multiplayer game was created in collaboration with a friend at my university.  My main responsibility was to utilize NodeJS to develop the back-end server.  It incorporated WebSocket functionality to establish real-time multiplayer communication.  I designed and developed multiple algorithms to manipulate data structures in serving WebSocket requests.  Additionally, it uses a MySQL database that I designed and constructed for data storage and retrieval.",
-        dateCreated: "Aug 10, 2021",
-        link: "https://www.programmersports.com",
-        img:"./assets/portfolio/KYXAGameWebsocketsAPI.png"
-    }
-]
+  {
+    id: 1,
+    title: "Task-Manager API",
+    description:
+      "A designed, developed, and deployed complete Restful API that allows a user to create accounts and store personal tasks in a real-time database.  It utilizes React.js as the front-end framework with Node.js running an Express.js server on the backend.  It interacts with MongoDB for data storage and retrieval.",
+    dateCreated: "Feb 20, 2021",
+    link: "https://github.com/ethancanne/task-manager-api",
+    img: "./assets/portfolio/TaskManagerAPI.png",
+  },
+  {
+    id: 2,
+    title: "KYXA Game Websockets API",
+    description:
+      "This online multiplayer game was created in collaboration with a friend at my university.  My main responsibility was to utilize NodeJS to develop the back-end server.  It incorporated WebSocket functionality to establish real-time multiplayer communication.  I designed and developed multiple algorithms to manipulate data structures in serving WebSocket requests.  Additionally, it uses a MySQL database that I designed and constructed for data storage and retrieval.",
+    dateCreated: "Aug 10, 2021",
+    link: "https://www.programmersports.com",
+    img: "./assets/portfolio/KYXAGameWebsocketsAPI.png",
+  },
+];
 
 export const fullstack = [
-    {
-        id: 1,
-        title: "Contact-Manager Website",
-        description: "This full-stack website was developed as a personal project using React.js as the front-end framework and Node.js as the back-end language.  It utilizes a Rest API that that interacts with a MongoDB database for storage and retrival of custom Contacts.",
-        dateCreated: "Jan 20, 2021",
-        link: "https://github.com/ethancanne/contact-manager",
-        img:"./assets/portfolio/ContactManagerWebsite.png"
-    },
-    {
-        id: 2,
-        title: "Weather Application",
-        description: "This full-stack website allows users to search for and retrieve weather information in a specified area using a third-party weather API.  It utilizes Handlebars in the front-end and Node.js in the back end, as well as a clean and modern interface.",
-        dateCreated: "Dec 14, 2020",
-        link: "https://cannelongo-weather-app.herokuapp.com/",
-        img:"./assets/portfolio/WeatherApplication.png"
-    }
-]
+  {
+    id: 1,
+    title: "Contact-Manager Website",
+    description:
+      "This full-stack website was developed as a personal project using React.js as the front-end framework and Node.js as the back-end language.  It utilizes a Rest API that that interacts with a MongoDB database for storage and retrival of custom Contacts.",
+    dateCreated: "Jan 20, 2021",
+    link: "https://github.com/ethancanne/contact-manager",
+    img: "./assets/portfolio/ContactManagerWebsite.png",
+  },
+  {
+    id: 2,
+    title: "Weather Application",
+    description:
+      "This full-stack website allows users to search for and retrieve weather information in a specified area using a third-party weather API.  It utilizes Handlebars in the front-end and Node.js in the back end, as well as a clean and modern interface.",
+    dateCreated: "Dec 14, 2020",
+    link: "https://cannelongo-weather-app.herokuapp.com/",
+    img: "./assets/portfolio/WeatherApplication.png",
+  },
+];
 
 export const mobile = [
-    {
-        id: 1,
-        title: "Prevymis Quest iPad OS App",
-        description: "I had the oppurtunity to work with a client to develop, deliver and distribute a complete iPad OS application. This application is similar to an escape room game, which was built for the purpose of training employees in a Pharmasutical company.  The design had been provided and I was tasked to take this idea and make it into a reality using Xcode and Swift.",
-        dateCreated: "Oct 10, 2019",
-        link: "https://appadvice.com/game/app/prevymis-quest/1473006753",
-        img:"./assets/portfolio/PrevymisQuest.jpeg"
-    },
-    {
-        id: 2,
-        title: "Positage Social Media Application",
-        description: "This application was developed as a personal project, which taught me the essential skills of iOS app development and design.  It was built using Xcode and Swift, using Firebase and Firestore for authentication and data storage.",
-        dateCreated: "June 25, 2020",
-        link: "https://github.com/ethancanne/Positage",
-        img:"./assets/portfolio/PositageApp.png"
-    },
-    {
-        id: 3,
-        title: "FocalPoint Cross-Platform Application",
-        description: "FocalPoint is a cross-platform application built using the React Native framework.  It features a modern design and functionality that helps people focus and keep track of important tasks throughout their day.",
-        dateCreated: "Dec 10, 2021",
-        link: "https://github.com/ethancanne/FocalPoint",
-        img:"./assets/portfolio/FocalPointApp.png"
-    },
-
-]
+  {
+    id: 1,
+    title: "Prevymis Quest iPad OS App",
+    description:
+      "I had the oppurtunity to work with a client to develop, deliver and distribute a complete iPad OS application. This application is similar to an escape room game, which was built for the purpose of training employees in a Pharmasutical company.  The design had been provided and I was tasked to take this idea and make it into a reality using Xcode and Swift.",
+    dateCreated: "Oct 10, 2019",
+    link: "https://appadvice.com/game/app/prevymis-quest/1473006753",
+    img: "./assets/portfolio/PrevymisQuest.jpeg",
+  },
+  {
+    id: 2,
+    title: "Positage Social Media Application",
+    description:
+      "This application was developed as a personal project, which taught me the essential skills of iOS app development and design.  It was built using Xcode and Swift, using Firebase and Firestore for authentication and data storage.",
+    dateCreated: "June 25, 2020",
+    link: "https://github.com/ethancanne/Positage",
+    img: "./assets/portfolio/PositageApp.png",
+  },
+  {
+    id: 3,
+    title: "FocalPoint Cross-Platform Application",
+    description:
+      "FocalPoint is a cross-platform application built using the React Native framework.  It features a modern design and functionality that helps people focus and keep track of important tasks throughout their day.",
+    dateCreated: "Dec 10, 2021",
+    link: "https://github.com/ethancanne/FocalPoint",
+    img: "./assets/portfolio/FocalPointApp.png",
+  },
+];
 
 export const design = [
-    {
-        id: 1,
-        title: "Study Spaces Web App Design",
-        description: "This design was created for a web-application that is currently being developed in my capstone course.  I am currently a part of a team that will take this mockup and covert it into an actual design using Sass for the Study Spaces web-application.  I utilized the powerful design capabilities of PowerPoint to develop these mockup images.",
-        dateCreated: "Oct 15, 2021",
-        link: "https://drive.google.com/file/d/19VRFuV8CjMV2ZKxeTueYbw5UiesAqnVD/view?usp=sharing",
-        img:"./assets/portfolio/StudySpacesUI.png"
-    },
-    {
-        id: 2,
-        title: "Student Information System Design",
-        description: "I created this design as a graded submission for a Software Development class.  It encompasses the UI Design of a student information system.  I utilized the powerful design capabilities of PowerPoint to develop these mockup images. ",
-        dateCreated: "April 2, 2021",
-        link: "https://drive.google.com/file/d/15pxKETXxZZqGtObxfptvrCpKkTSnobBQ/view?usp=sharing",
-        img:"./assets/portfolio/SISUI.png"
-    },
-]
+  {
+    id: 1,
+    title: "Study Spaces Web App Design",
+    description:
+      "This design was created for a web-application that is currently being developed in my capstone course.  I am currently a part of a team that will take this mockup and covert it into an actual design using Sass for the Study Spaces web-application.  I utilized the powerful design capabilities of PowerPoint to develop these mockup images.",
+    dateCreated: "Oct 15, 2021",
+    link: "https://drive.google.com/file/d/19VRFuV8CjMV2ZKxeTueYbw5UiesAqnVD/view?usp=sharing",
+    img: "./assets/portfolio/StudySpacesUI.png",
+  },
+  {
+    id: 2,
+    title: "Student Information System Design",
+    description:
+      "I created this design as a graded submission for a Software Development class.  It encompasses the UI Design of a student information system.  I utilized the powerful design capabilities of PowerPoint to develop these mockup images. ",
+    dateCreated: "April 2, 2021",
+    link: "https://drive.google.com/file/d/15pxKETXxZZqGtObxfptvrCpKkTSnobBQ/view?usp=sharing",
+    img: "./assets/portfolio/SISUI.png",
+  },
+];
